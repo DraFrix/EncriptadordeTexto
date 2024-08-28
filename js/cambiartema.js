84d28672c8ec5123c9ecdd4ca3e9c7e963b199d4
DraFrix <davidruizr146@gmail.com>
2024-08-27 22:05:45 -0500

    Segunda carga

--- js/cambiartema.js
+++ js/cambiartema.js
@@ -1,6 +1,5 @@
 // Seleccionamos los elementos
 const body = document.body;
-const header = document.body.header;
 const iconoSol = document.getElementById('icono-sol');
 const iconoLuna = document.getElementById('icono-luna');
 const themeToggle = document.querySelector('.theme-toggle');
@@ -19,13 +18,11 @@ if (modoGuardado === 'oscuro') {
 themeToggle.addEventListener('click', () => {
     if (body.classList.contains('light-mode')) {
         body.classList.replace('light-mode', 'dark-mode');
-        header.classList.replace('light-mode', 'dark-mode');
         iconoSol.classList.add('invisible');
         iconoLuna.classList.remove('invisible');
         localStorage.setItem('modo-tema', 'oscuro');
     } else {
         body.classList.replace('dark-mode', 'light-mode');
-        header.classList.replace('dark-mode', 'light-mode');
         iconoSol.classList.remove('invisible');
         iconoLuna.classList.add('invisible');
         localStorage.setItem('modo-tema', 'claro');
